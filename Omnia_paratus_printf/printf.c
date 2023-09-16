ecc64d0e4739fb98c9cd42b9cba02950e8f89dee
TsTsoundz <adacheprince@gmail.com>
2023-09-17 00:47:42 +0100

    Success

--- Omnia_paratus_printf/printf.c
+++ Omnia_paratus_printf/printf.c
@@ -1,28 +1,50 @@
-#Include "main.h"
+#include "main.h"
+#include <limits.h>
+#include <stdio.h>
 
 /**
- * _printf * function that produces outputaccending in a format
- * @format: format accending to which argument will be printed
- * Return: return number of characters printed
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
  */
-int _print(const char *format, ...)
+int _printf(const char *format, ...)
 {
-	
-	int count = 0;
-	va_list args;
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
 
-	 if (*format == NULL)
-		 return (-1);
+	register int count = 0;
 
-	 va_start(args, formart);
-	 
-	 while (formart ≠ '\0')
-	 {
-		 if (formart ≠ '%')
-		 {
-			 write(1, formart, 1)
-		 }
-		 formart++;
-	 }
-	 return (count);
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
+	{
+		if (*p == '%')
+		{
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
+	}
+	_putchar(-1);
+	va_end(arguments);
+	return (count);
+
 }
